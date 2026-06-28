6554a02bd477feed80fac77a65804ede5664da7b
connorCod <connorjhruby@gmail.com>
2026-06-28 14:56:47 -0600

    practicing seperate file compiling and linking of functions

---
+++ Ch_2/add.cpp
@@ -0,0 +1,4 @@
+int add_file(int x, int y)
+{
+    return x + y;
+}
---
+++ Ch_2/add_num.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int add_file(int x, int y);
+
+int main()
+{
+    std::cout << add_file(3,4) << '\n';
+    return 0;    
+}
