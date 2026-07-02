4a09e11dc02a0e8d2b6a2cb25489ac97a413c20c
connorCod <connorjhruby@gmail.com>
2026-07-01 22:57:07 -0600

    split out main.cpp and used forward declaration to reference functions split across files

---
+++ Ch_2/ch2_quiz/io.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int readNumber()
+{
+    std::cout << "Enter a number: ";
+    int num;
+    std::cin >> num;
+
+    return num;
+}
+
+void writeAnswer(int num)
+{
+    std::cout << "Your two numbers added together are: " << num << '\n';
+}
--- Ch_2/ch2_quiz/main.cpp
+++ Ch_2/ch2_quiz/main.cpp
@@ -1,18 +1,7 @@
 #include <iostream> 
 
-int readNumber()
-{
-    std::cout << "Enter a number: ";
-    int num;
-    std::cin >> num;
-
-    return num;
-}
-
-void writeAnswer(int num)
-{
-    std::cout << "Your two numbers added together are: " << num << '\n';
-}
+int readNumber();
+void writeAnswer(int num);
 
 int main()
 {
